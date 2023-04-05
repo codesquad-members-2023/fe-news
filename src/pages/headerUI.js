a2f303dca62471b00c255c70952aa7c10c36b1ee
Hannah Ahn <ahnlook90@gmail.com>
2023-04-05 13:36:19 +0900

    css styles to header

--- src/pages/headerUI.js
+++ src/pages/headerUI.js
@@ -2,31 +2,50 @@ import { createNode } from '../script/utils.js'
 import HeaderHandler from './header.js'
 
 class HeaderUI extends HTMLElement {
+  #headerData
+  #container
+
   constructor() {
     super()
+
+    const handler = new HeaderHandler()
+    this.#headerData = handler.getData()
   }
 
   connectedCallback() {
-    const handler = new HeaderHandler()
-    const handlerData = handler.getData()
+    this.#container = createNode('div')
+    this.appendChild(this.#container)
 
+    this.#createTitleSection()
+    this.#createDateSection()
+  }
+
+  #createTitleSection() {
     const container = createNode('div')
-    this.appendChild(container)
 
     const icon = createNode('img')
-    icon.src = handlerData.icon
+    icon.src = this.#headerData.icon
     icon.alt = 'logo'
     container.appendChild(icon)
 
     const title = createNode('span')
-    title.textContent = handlerData.title
+    title.textContent = this.#headerData.title
     title.classList.add('display')
     container.appendChild(title)
 
+    this.#container.appendChild(container)
+  }
+
+  #createDateSection() {
+    const container = createNode('div')
+    container.classList.add('todayDate')
+
     const today = createNode('span')
-    today.textContent = handlerData.date
+    today.textContent = this.#headerData.date
     today.classList.add('body-md')
     container.appendChild(today)
+
+    this.#container.appendChild(container)
   }
 }
 
