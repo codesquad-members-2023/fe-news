0da8c039d2e3ab85c143191aa0c47959c0134607
JangHoonSeok <orirori512@naver.com>
2023-04-05 20:02:56 +0900

    ✨ feat: svg 파일 create를 위한 util 생성

--- js/utils/dom.js
+++ js/utils/dom.js
@@ -1,6 +1,12 @@
-export const setAttributes = (element, attributes = {}) => {
+const setAttributes = (element, attributes = {}) => {
   for (const attributeKey in attributes) {
     element.setAttribute(attributeKey, attributes[attributeKey]);
+  }
+};
+
+const setAttributesNS = (element, attributes = {}) => {
+  for (const attributeKey in attributes) {
+    element.setAttributeNS(attributeKey, attributes[attributeKey]);
   }
 };
 
@@ -8,4 +14,10 @@ export const createElement = (elementName, options = {}) => {
   const $element = document.createElement(elementName);
   setAttributes($element, options);
   return $element;
+};
+
+export const createSVGElement = (svgURL, elementName, options = {}) => {
+  const $svgElement = document.createElementNS(svgURL, elementName);
+  setAttributesNS($svgElement, options);
+  return $svgElement;
 };
