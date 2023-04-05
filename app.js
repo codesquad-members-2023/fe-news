0a1989a07ea7b9463cac07276a778040d2e75287
JaydenLee1116 <vv55adss@gmail.com>
2023-04-05 14:11:26 +0900

    chore: Reapply gitignore
    gitignore 재적용

--- app.js
+++
@@ -1,8 +0,0 @@
-import { HeaderComponent } from './src/components/header/HeaderComponent.js';
-class App {
-    constructor(appRoot) {
-        const header = new HeaderComponent();
-        appRoot.appendChild(header.element);
-    }
-}
-const app = new App(document.querySelector('#app'));
