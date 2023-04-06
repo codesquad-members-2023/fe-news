2efd7de0d086c4f7cc0dc281c4b504a50c74fa95
YouSooA <kinsk2839@naver.com>
2023-04-06 19:28:56 +0900

    feat: App 클래스에 MainContent 클래스 적용

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,18 @@
 import Header from './components/header.js';
+import MainContent from './components/mainContent/mainContent.js';
 
 export default class App {
   constructor($targetEle) {
     this.$targetEle = $targetEle;
 
+    this.header = new Header(this.$targetEle);
+    this.mainContent = new MainContent(this.$targetEle);
+
     this.initRender();
   }
 
   initRender() {
-    new Header(this.$targetEle).initRender();
+    this.header.initRender();
+    this.mainContent.initRender();
   }
 }
