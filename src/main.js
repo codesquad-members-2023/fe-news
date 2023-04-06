1d59b16b81b2208c8121b14424f5400438706d47
sarang_daddy <sarang_daddy@naver.com>
2023-04-06 16:05:15 +0900

    [Feat] 뉴스스탠드 header JS 동적 기능 추가
    - JS로 news-stand header 구현

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log("1");
+import { headerEl } from "./components/header/headerView.js";
+
+const mainTag = document.createElement("main");
+mainTag.classList.add("news-stand");
+mainTag.appendChild(headerEl);
+
+document.body.appendChild(mainTag);
