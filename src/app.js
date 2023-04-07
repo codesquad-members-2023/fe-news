c37fab3b948023a45cb0feab33c9c1b4a4f08a0b
Hannah Ahn <ahnlook90@gmail.com>
2023-04-07 11:12:05 +0900

    add style for grid cell

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import './pages/headerUI.js'
 import HeaderHandler from './pages/header.js'
 import './pages/mainGridBlockUI.js'
+import MainGrid from './pages/mainGrid.js'
 
 const header = new HeaderHandler()
+const mainGrid = new MainGrid()
