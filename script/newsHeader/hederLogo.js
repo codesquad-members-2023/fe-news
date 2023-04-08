2fea409c3e2993c01bea962e1db1e11618fff62e
안예림 <an-yelim@an-yelim-ui-MacBookAir.local>
2023-04-08 22:54:53 +0900

    feat: 뉴스 스탠드 로고를 클릭하면 새로고침 제작 완료
    js로 새로 고침을 만들까하다가, html에 직접 제어가 가능하게 하면, 좀 더 가벼워진다는 소리를 들어서 간단한 거는 html에 직접 기능을 주었습니다.

--- script/newsHeader/hederLogo.js
+++ script/newsHeader/hederLogo.js
@@ -1,6 +1,6 @@
 export default class HeaderLogo {
   insertLogo() {
-    return `<img class = "logo_img" src="assets/newsPaper.svg">`;
+    return `<a onClick="window.location.reload()" style="cursor: pointer;"><img class = "logo_img" src="assets/newsPaper.svg"></a>`;
   }
   insertLogoText() {
     return `<span class= "logo_text">뉴스스탠드</span>`;
