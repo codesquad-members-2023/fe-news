4436f825fc99bf52dedd4d07ca0503f6cf518fb5
saseung <seung0122@naver.com>
2023-04-10 05:22:37 +0900

    style: 스타일 리셋 파일, header 클래스명 변경, 전역 스타일 코드 추가

--- src/pages/header.js
+++ src/pages/header.js
@@ -5,7 +5,7 @@ export class Header {
     const newElement = document.createElement('div');
     // todo 날짜 데이터 받는 함수 만들어서 집어넣기
     const template = `
-    <div class="news__header">
+    <div class="header">
       <div class="title">
         <img src="../src/assets/images/newsstand_logo.svg"/>
         <span>뉴스스탠드</span>
