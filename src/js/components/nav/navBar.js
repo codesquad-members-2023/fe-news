21bcbf8445f4cc5bef74df574e290e154edd856d
YouSooA <kinsk2839@naver.com>
2023-04-10 16:00:46 +0900

    feat: NavBar 클래스 생성
    - nav태그 안에 들어갈 bar 컴포넌트 생성
    
    - 기본적인 component 메서드로 구성

---
+++ src/js/components/nav/navBar.js
@@ -0,0 +1,44 @@
+export default class NavBar {
+  constructor($parent, props) {
+    this.$parent = $parent;
+    this.$ele = document.createElement('div');
+    this.$ele.className = 'nav-bar';
+
+    this.props = props;
+    this.state = {};
+
+    this.initState();
+  }
+
+  mount() {
+    this.render();
+    this.$parent.insertAdjacentElement('beforeend', this.$ele);
+  }
+
+  render() {
+    this.$ele.innerHTML = this.template();
+  }
+
+  template() {
+    const { press, headLines } = this.props;
+    // * 2개의 headline으로 ui 확인
+    const fakeHeadLines = headLines.slice(0, 2);
+
+    return /* html */ `
+      <a href="" class="nav-bar__press">${press}</a>
+      <div class="nav-bar__headLine-wrapper">
+        ${fakeHeadLines.map((headLine) => `<a href="">${headLine}</a>`).join('')}
+      </div>
+    `;
+  }
+
+  initState() {
+    const { press, headLines } = this.props;
+
+    this.setState({ press, headLines });
+  }
+
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+  }
+}
