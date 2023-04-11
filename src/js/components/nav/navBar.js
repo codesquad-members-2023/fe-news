3c5451b8117985835e2845e7abc1d8d17cd764f2
YouSooA <kinsk2839@naver.com>
2023-04-11 14:50:50 +0900

    feat: nav bar의 headline 5초마다 자동으로 무한 롤링하는 기능 구현

--- src/js/components/nav/navBar.js
+++ src/js/components/nav/navBar.js
@@ -1,5 +1,9 @@
+import { $ } from '../../utils/dom.js';
+
 export default class NavBar {
   #startIdx = 0;
+
+  #startTime = null;
 
   constructor($parent, props) {
     this.$parent = $parent;
@@ -15,10 +19,43 @@ export default class NavBar {
   mount() {
     this.render();
     this.$parent.insertAdjacentElement('beforeend', this.$ele);
+    this.setEvent();
+    this.setAutoRolling();
   }
 
   render() {
     this.$ele.innerHTML = this.template();
+  }
+
+  setAutoRolling() {
+    const { navBarData } = this.props;
+    const { headLines } = navBarData;
+    const $headLineWrapper = $({
+      selector: '.nav-bar__headLine-wrapper',
+      parent: this.$ele
+    });
+
+    const autoRolling = (timestamp) => {
+      if (!this.#startTime) this.#startTime = timestamp;
+
+      const progress = timestamp - this.#startTime;
+
+      if (progress > 5000) {
+        this.setState({ currentIdx: (this.state.currentIdx + 1) % 5 });
+        const { currentIdx } = this.state;
+        // ! 렌더링 관련 부분 리팩토링 => headLineWrapper 자식 요소들 component로 만들 예정
+        $headLineWrapper.insertAdjacentHTML(
+          'beforeend',
+          `<a href="">${headLines[currentIdx]}</a>`
+        );
+
+        $headLineWrapper.classList.add('rolling');
+        this.#startTime = null;
+      }
+      requestAnimationFrame(autoRolling);
+    };
+
+    requestAnimationFrame(autoRolling);
   }
 
   template() {
@@ -42,5 +79,19 @@ export default class NavBar {
 
   setState(newState) {
     this.state = { ...this.state, ...newState };
+  }
+
+  setEvent() {
+    const $headLineWrapper = $({
+      selector: '.nav-bar__headLine-wrapper',
+      parent: this.$ele
+    });
+
+    $headLineWrapper.addEventListener('transitionend', ({ currentTarget }) => {
+      // ! 렌더링 관련 부분 리팩토링
+      const firstElement = currentTarget.firstElementChild;
+      currentTarget.removeChild(firstElement);
+      currentTarget.classList.remove('rolling');
+    });
   }
 }
