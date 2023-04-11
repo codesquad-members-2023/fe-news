19330aa234fcf644c0a214341b98127d0128e271
YouSooA <kinsk2839@naver.com>
2023-04-11 19:58:29 +0900

    feat: MainContentHeader 클래스에서 press tab 버튼 이벤트 설정 및 constructor 파라미터 추가
    - 부모의 상태(props)를 받는 파라미터 추가

--- src/js/components/mainContent/index.js
+++ src/js/components/mainContent/index.js
@@ -6,11 +6,41 @@ export default class MainContent {
     this.$ele = document.createElement('main');
     this.$ele.id = 'main-content';
 
-    this.header = new MainContentHeader(this.$ele);
+    this.state = {};
+    this.initState();
+
+    this.header;
   }
 
   mount() {
+    const { isAllPressTab, isGridTab } = this.state;
+    const { pressTabHandler } = this;
+
+    this.header = new MainContentHeader(this.$ele, {
+      isAllPressTab,
+      isGridTab,
+      pressTabHandler: pressTabHandler.bind(this)
+    });
     this.header.mount();
+
     this.$parent.insertAdjacentElement('beforeend', this.$ele);
+  }
+
+  update({ newState }) {
+    this.setState(newState);
+    this.header.update({ newProps: this.state });
+  }
+
+  initState() {
+    this.state = { isAllPressTab: true, isGridTab: true };
+  }
+
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+  }
+
+  pressTabHandler() {
+    const { isAllPressTab } = this.state;
+    this.update({ newState: { isAllPressTab: !isAllPressTab } });
   }
 }
--- src/js/components/mainContent/mainContentHeader.js
+++ src/js/components/mainContent/mainContentHeader.js
@@ -1,13 +1,28 @@
 export default class MainContentHeader {
-  constructor($parent) {
+  #imgSrc = {
+    listBlue: '../../../src/images/list_blue.svg',
+    listGray: '../../../src/images/list_gray.svg',
+    gridBlue: '../../../src/images/grid_blue.svg',
+    gridGray: '../../../src/images/grid_gray.svg'
+  };
+
+  constructor($parent, props) {
     this.$parent = $parent;
     this.$ele = document.createElement('header');
     this.$ele.className = 'main-content__header';
+
+    this.props = props;
   }
 
   mount() {
     this.render();
+    this.setEvent();
     this.$parent.insertAdjacentElement('afterbegin', this.$ele);
+  }
+
+  update({ newProps }) {
+    this.updateProps(newProps);
+    this.render();
   }
 
   render() {
@@ -15,15 +30,38 @@ export default class MainContentHeader {
   }
 
   template() {
+    const { isAllPressTab, isGridTab } = this.props;
+    const { listBlue, listGray, gridBlue, gridGray } = this.#imgSrc;
+
     return /* html */ `
       <div class="press-tab">
-        <span class="press-tab-btn active">전체 언론사</span>
-        <span class="press-tab-btn">내가 구독한 언론사</span>
+        <span class="press-tab-btn press-tab__all ${
+          isAllPressTab ? 'active' : ''
+        }">전체 언론사</span>
+        <span class="press-tab-btn press-tab__subscribed ${
+          !isAllPressTab ? 'active' : ''
+        }">내가 구독한 언론사</span>
       </div>
       <div class="show-tab">
-        <img class="show-tab__list" src="../../../src/images/list_gray.svg">
-        <img class="show-tab__grid" src="../../../src/images/grid_blue.svg">
+        <img class="show-tab-btn show-tab__list" src="${isGridTab ? listBlue : listGray}">
+        <img class="show-tab-btn show-tab__grid" src="${!isGridTab ? gridBlue : gridGray}">
       </div>
     `;
+  }
+
+  updateProps(newProps) {
+    this.props = { ...this.props, ...newProps };
+  }
+
+  setEvent() {
+    this.$ele.addEventListener('click', ({ target }) => {
+      const { isAllPressTab, pressTabHandler } = this.props;
+      const targetClassList = target.classList;
+      if (
+        (targetClassList.contains('press-tab__all') && !isAllPressTab) ||
+        (targetClassList.contains('press-tab__subscribed') && isAllPressTab)
+      )
+        pressTabHandler();
+    });
   }
 }
