bee5f88798694e225afdd35ce1dad32a05e2ed42
NANII <breadnanii@gmail.com>
2023-04-12 16:14:05 +0900

    feat: Data Store 적용
    - Data store 에서 모든 데이터를 패치한 뒤
    - 롤링영역, 뷰영역 각각의 스토어에 생성하여 전달하고자 했으나 아직 설계중이므로 우선 매개변수로 데이터를 전달함

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,34 @@
-import { CONSTANTS, autoAnimationInfo } from './core/constants.js';
+import { CONSTANTS, API_URL, autoAnimationInfo } from './core/constants.js';
 import systemTimeOption from './utils/systemTime.js';
 import Header from './components/header/header.js';
 import RollingBar from './components/section/rollingBar.js';
 import main from './components/main/viewContainer/mainView.js';
+import fetchData from './utils/fetch.js';
+import { DataStore } from './stores/dataStore.js';
+import { RollingStore } from './stores/rollingStore.js'
+import { ViewStore } from './stores/viewStore.js'
 
 const app = async () => {
-  const root = document.querySelector('#root');
-  const header = Header(CONSTANTS['NEWS_STAND'], systemTimeOption);
-  const section = new RollingBar(
-    CONSTANTS['ROLLING_LINK_PRESS'],
-    { autoAnimationInfo },
-    'left',
-    'right',
-  ).render();
-  const mainView = main();
+  const responseData = await fetchData(API_URL['rolling'], API_URL['media']);
+  DataStore.dispatch('DELIVER_DATA', { responseData });
+  DataStore.subscribe(() => {
+    const rollingData = { ...DataStore.getState().rollingData };
+    const viewData = { ...DataStore.getState().mediaData };
+    // RollingStore.dispatch('START_ROLLINGBAR', { rollingData });
+    // ViewStore.dispatch('SET_GRIDVIEW', { viewData });
 
-  root.append(header, section, mainView);
-  // const [rollingData, mediaData] = await getAllData(API_URL['rolling'], API_URL['media']);
+    const root = document.querySelector('#root');
+    const header = Header(CONSTANTS['NEWS_STAND'], systemTimeOption);
+    const section = new RollingBar(
+      rollingData,
+      CONSTANTS['ROLLING_LINK_PRESS'],
+      { autoAnimationInfo },
+      'left',
+      'right',
+    ).render();
+    const mainView = main();
+    root.append(header, section, mainView);
+  });
 };
 
 app();
