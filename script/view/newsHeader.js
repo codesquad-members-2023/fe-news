4191e622168b131862e5aac308e096523485c421
yesolyo <ahnyelim77@gmail.com>
2023-04-12 20:14:00 +0900

    refactor: header 부분의 날짜 함수를 간략화
    toLocaleDateString을 이용하여 날짜 함수를 간략화 하였습니다.

--- script/view/newsHeader.js
+++ script/view/newsHeader.js
@@ -1,11 +1,7 @@
 const insertDate = () => {
-  const days = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
-  let today = new Date();
-  let year = today.getFullYear();
-  let month = (today.getMonth() + 1).length == 1 ? "0" + today.getMonth() + 1 : today.getMonth() + 1;
-  let date = today.getDate().length == 1 ? "0" + today.getDate() : today.getDate();
-  let day = days[today.getDay()];
-  return `<span class="date_text">${year}. ${month}. ${date}. ${day}</span>`;
+  const today = new Date();
+  const options = { weekday: "long", year: "numeric", month: "2-digit", day: "2-digit" };
+  return `<span class="date_text">${today.toLocaleDateString("ko-KO", options)}</span>`;
 };
 const newsHeaderTemplate = () =>
   `<div class="news__header">
