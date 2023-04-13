01992445f5ef2d68ea28c5de1725d006ec66de33
yesolyo <ahnyelim77@gmail.com>
2023-04-13 15:24:57 +0900

    feat: 뉴스 회사 로고 모음 위에 있는 바 완료

--- script/main.js
+++ script/main.js
@@ -1,9 +1,11 @@
 import { fetchManager } from "./controller/fetchManager.js";
 import { viewNewsHeader } from "./view/newsHeader.js";
 import { rollingData, viewRollingBar } from "./view/rollingBar.js";
+import { viewNewsCompanyBar } from "./view/newsCompany.js";
 const main = () => {
   viewNewsHeader();
   viewRollingBar();
+  viewNewsCompanyBar();
   fetchManager();
 };
 
--- script/view/newsCompany.js
+++ script/view/newsCompany.js
@@ -1,15 +1,20 @@
-const newsCompanyTemplate = `
-<div class="news-company__choice">
-<span class="choice_all">전체 언론사</span>
-<span class="choice_subscribe">내가 구독한 언론사</span>
+const newsCompanyTemplate = () => `
+<div class = "news-company__bar">
+<div class="company__choice">
+<span class="company__choice_all">전체 언론사</span>
+<span class="company__choice_subscribe">내가 구독한 언론사</span>
 </div>
-<div class = "news-company__view">
-<img class="view_detail">
-</img>
+<div class = "company__view">
+<img class= "company__view_detail" src="assets/viewDetail.svg"/>
+<img class=" company__view_logo" src="assets/viewLogo.svg"/>
+</div>
+</div>
 `;
-const viewAllNewsCommpany = () => {
+const viewNewsCompanyBar = () => {
   const root = document.querySelector(".root");
-  const newsRollingBar = document.createElement("section");
-  root.appendChild(newsRollingBar);
-  newsRollingBar.innerHTML = rollingBarTemplate();
+  const newsCompanyBar = document.createElement("section");
+  root.appendChild(newsCompanyBar);
+  newsCompanyBar.innerHTML = newsCompanyTemplate();
 };
+
+export { viewNewsCompanyBar };
