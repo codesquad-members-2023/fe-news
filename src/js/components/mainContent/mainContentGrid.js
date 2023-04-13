60698aa1150ba3a3240dcbc5f490ef57b6dc73b1
YouSooA <kinsk2839@naver.com>
2023-04-13 18:07:58 +0900

    feat: MainContentGrid 클래스 생성
    - 전체 언론사 그리드 컴포넌트와 구독한 언론사 그리드 컴포넌트 만들 때 사용

---
+++ src/js/components/mainContent/mainContentGrid.js
@@ -0,0 +1,24 @@
+import PressGrid from './pressGrid.js';
+
+export default class MainContentGrid {
+  #gridItemCount = 24;
+
+  constructor($parent, props) {
+    this.$parent = $parent;
+    this.$ele = document.createElement('section');
+    this.$ele.className = 'main-content__grid';
+
+    this.props = props;
+
+    this.pressGridCollection;
+  }
+
+  mount() {
+    const { pressChucks } = this.props;
+
+    this.pressGridCollection = pressChucks.map((chuck) => new PressGrid(this.$ele, { gridItemsData: chuck }));
+    this.pressGridCollection.forEach((pressGrid) => pressGrid.mount());
+
+    this.$parent.insertAdjacentElement('beforeend', this.$ele);
+  }
+}
