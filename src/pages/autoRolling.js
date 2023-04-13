2e0ee6347906b350b5b1a3038884ce2bcaeeee41
saseung <seung0122@naver.com>
2023-04-13 15:10:08 +0900

    feat: 템플릿 함수 분리 후 fetch해온 데이터 롤링 박스에 추가

--- src/pages/autoRolling.js
+++ src/pages/autoRolling.js
@@ -1,23 +1,32 @@
-export class AutoRolling {
-  constructor() {}
-  viewRender() {
-    const newElement = document.createElement('div');
-    const template = `
+class AutoRolling {
+  constructor(data) {
+    this.leftData = data.leftRollingData;
+    this.rightData = data.rightRollingData;
+  }
+
+  template() {
+    return `
     <div class="auto-rolling">
       <div class="rolling__container">
         <div class="media">연합뉴스</div>
-        <div class="issue">[1보] 김기현,안철수,황교안</div>
+        <div class="issue">${this.leftData}</div>
       </div>
       <div class="rolling__container">
       <div class="media">연합뉴스</div>
-      <div class="issue">[1보] 김기현,안철수,황교안</div>
+      <div class="issue">${this.rightData}</div>
     </div>
     </div>
     `;
-    newElement.innerHTML = template;
+  }
+
+  viewRender() {
+    const newElement = document.createElement('div');
+    newElement.innerHTML = this.template();
     return newElement;
   }
 }
+
+export { AutoRolling };
 
 // todo issue 데이터 할당
 // viewRender항목 utils로 분리해서 재활용가능하게 쓸수있는지 고민해보기
