cac83ffd1ff7e12ffd4426ec6379507b730bd73b
poco <godiva140@gmail.com>
2023-04-13 16:48:47 +0900

    refactor: constant 파일 분리 및 파일명, 변수명 변경

---
+++ src/js/constant/api.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = 'http://localhost:3001';
+
+export const API_PATH = Object.freeze({
+  HEADLINE: '/headline',
+});
+
+export { API_BASE_URL, API_PATH };
---
+++ src/js/constant/dom.js
@@ -0,0 +1,22 @@
+const NS_HEADER_INFO = Object.freeze({
+  LOGO_IMG_SRC: '/src/asset/headerIcon.svg',
+  IMG_ALT: 'newspaper',
+  TITLE: '뉴스스탠드',
+});
+
+const NS_HEADLINE_INFO = Object.freeze({
+  TITLE: '연합뉴스',
+  HEADLINE_LENGTH: 5,
+  ANIMATION: {
+    TRANSITION_DURATION: 500,
+    LEFT_DELAY_DURATION: 3000,
+    RIGHT_DELAY_DURATION: 1000,
+    HEADLINE_LENGTH: 5,
+  },
+});
+
+const REFERENCE = Object.freeze({
+  NS_CONTAINER: document.querySelector('.newsstand_container'),
+});
+
+export { NS_HEADER_INFO, NS_HEADLINE_INFO, REFERENCE };
