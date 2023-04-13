f597fe61c159be984c3d38f4816c6f667d5dea21
YouSooA <kinsk2839@naver.com>
2023-04-13 21:18:28 +0900

    refactor: grid 아이콘 및 list 아이콘 이미지 경로를 절대 경로로 변경

--- src/js/components/mainContent/mainContentHeader.js
+++ src/js/components/mainContent/mainContentHeader.js
@@ -1,9 +1,9 @@
 export default class MainContentHeader {
   #imgSrc = {
-    listBlue: '../../../src/images/list_blue.svg',
-    listGray: '../../../src/images/list_gray.svg',
-    gridBlue: '../../../src/images/grid_blue.svg',
-    gridGray: '../../../src/images/grid_gray.svg'
+    listBlue: 'src/images/list_blue.svg',
+    listGray: 'src/images/list_gray.svg',
+    gridBlue: 'src/images/grid_blue.svg',
+    gridGray: 'src/images/grid_gray.svg'
   };
 
   constructor($parent, props) {
@@ -43,12 +43,8 @@ export default class MainContentHeader {
         }">내가 구독한 언론사</span>
       </div>
       <div class="show-tab">
-        <img class="show-tab-btn show-tab__list" src="${
-          activeShowTab === 'list' ? listBlue : listGray
-        }">
-        <img class="show-tab-btn show-tab__grid" src="${
-          activeShowTab === 'grid' ? gridBlue : gridGray
-        }">
+        <img class="show-tab-btn show-tab__list" src="${activeShowTab === 'list' ? listBlue : listGray}">
+        <img class="show-tab-btn show-tab__grid" src="${activeShowTab === 'grid' ? gridBlue : gridGray}">
       </div>
     `;
   }
