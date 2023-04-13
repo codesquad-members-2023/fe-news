22034eefaddaafe5b5193576a457d3be48c01e3e
yesolyo <ahnyelim77@gmail.com>
2023-04-13 10:39:33 +0900

    refactor : 중복 함수 제거

--- script/controller/controller.js
+++ script/controller/controller.js
@@ -3,25 +3,16 @@ const rollingController = () => {
   fetch("http://localhost:3001/rollingData")
     .then((res) => res.json())
     .then((data) => {
-      rollingRightData(data);
-      rollingLeftData(data);
+      deliverData(data, 0, data.length / 2, ".data_list_left");
+      deliverData(data, data.length / 2, data.length, ".data_list_right");
     })
     .catch((error) => console.error("Error:", error));
 };
-
-const rollingRightData = (data) => {
-  const leftData = [];
-  for (let i = 0; i < data.length / 2; i++) {
-    leftData.push(data[i].headLine);
+const deliverData = (data, num1, num2, className) => {
+  const newsData = [];
+  for (let i = num1; i < num2; i++) {
+    newsData.push(data[i].headLine);
   }
-  insertNewsData(leftData, ".data_list_left");
-};
-
-const rollingLeftData = (data) => {
-  const rightData = [];
-  for (let i = data.length / 2; i < data.length; i++) {
-    rightData.push(data[i].headLine);
-  }
-  insertNewsData(rightData, ".data_list_right");
+  insertNewsData(newsData, className);
 };
 export { rollingController };
