7ec0e813ba88ff6fd31be6e6977852747b6ad834
YouSooA <kinsk2839@naver.com>
2023-04-14 17:34:09 +0900

    refactor: app.js 삭제하고 app 함수 index.js로 이동

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
 import { $ } from './utils/dom.js';
-import app from './app.js';
+import Header from './components/header/index.js';
+import Nav from './components/nav/index.js';
+import MainContent from './components/mainContent/index.js';
+
+const app = async ($targetEle) => {
+  const header = new Header($targetEle);
+  const nav = new Nav($targetEle);
+  const mainContent = new MainContent($targetEle);
+
+  header.mount();
+  await nav.mount();
+  mainContent.mount();
+};
 
 app($({ selector: '#app' }));
