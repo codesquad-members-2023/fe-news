4556014c2eb5d614d6a8a890159b927de05ef5fc
JangHoonSeok <orirori512@naver.com>
2023-04-14 17:38:08 +0900

    ✨ feat: Entry Point Index -> App 으로 변경

---
+++ js/App.js
@@ -0,0 +1,27 @@
+import Header from './components/header.js';
+import AutoRolling from './components/autoRolling/autoRolling.js';
+import MainCommon from './components/main/MainCommon/mainCommon.js';
+import MainGrid from './components/main/MainAllGrid/mainAllGrid.js';
+import MainAllList from './components/main/MainAllLIst/mainAllList.js';
+import { createElement } from './utils/dom.js';
+// 애플리케이션의 root 컴포넌트.
+
+const App = () => {
+  const root = document.querySelector('#root');
+
+  const $header = Header();
+  const $autoRolling = AutoRolling();
+  const $main = createElement('main');
+  const [$mainHeader, $mainButtons] = MainCommon();
+  const $mainGrid = MainGrid($main);
+  $main.append($mainHeader, $mainButtons[0], $mainButtons[1], $mainGrid);
+
+  const documentFragment = new DocumentFragment();
+  documentFragment.append($header, $autoRolling, $main);
+
+  MainAllList($main);
+
+  root.appendChild(documentFragment);
+};
+
+App();
--- js/index.js
+++
@@ -1,4 +0,0 @@
-import App from './components/App.js';
-
-// 애플리케이션의 진입점(Entry Point) 파일
-App();
