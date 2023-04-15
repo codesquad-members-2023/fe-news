c96063017a46cf6041411fa32b6a9c905aa3c073
saseung <seung0122@naver.com>
2023-04-15 13:46:08 +0900

    refactor: header 클래스 함수로 분리, 현재 날짜 리턴하는 함수 구현

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,20 +1,31 @@
-export class Header {
-  constructor() {}
-
-  viewRender() {
-    const newElement = document.createElement('div');
-    // todo 날짜 데이터 받는 함수 만들어서 집어넣기
-    const template = `
-    <div class="header">
-      <div class="title">
-        <img src="../src/assets/images/newsstand_logo.svg"/>
-        <span>뉴스스탠드</span>
-      </div>
-      <div class="date">2023.02.10.금요일</div>
+const viewHeader = () => {
+  const newElement = document.createElement('div');
+  const template = `
+  <div class="header">
+    <div class="title">
+      <img src="../src/assets/images/newsstand_logo.svg"/>
+      <span>뉴스스탠드</span>
     </div>
-    `;
-    newElement.innerHTML = template;
+    <div class="date">${getCurrentDate()}</div>
+  </div>
+  `;
+  newElement.innerHTML = template;
 
-    return newElement;
-  }
-}
+  return newElement;
+};
+
+const getCurrentDate = () => {
+  const WEEK = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+  const date = new Date();
+  const dateInfo = {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate(),
+    weekDay: WEEK[date.getDay()],
+  };
+  const today = `${dateInfo.year}.${dateInfo.month}.${dateInfo.day}.${dateInfo.weekDay}`;
+
+  return today;
+};
+
+export { viewHeader };
