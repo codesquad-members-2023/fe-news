1f69a6a3a5bddd8a0a9cf90fbad805927af13df6
yousooa <kinsk2839@naver.com>
2023-04-16 16:47:21 +0900

    refactor: pressTab 컴포넌트와 showTab 컴포넌트 생성

--- src/js/components/mainContent/mainContentHeader.js
+++ src/js/components/mainContent/mainContentHeader.js
@@ -1,63 +1,22 @@
-import { tabStore } from '../../store/tabStore.js';
+import PressTab from './pressTab.js';
+import ShowTab from './showTab.js';
 
 export default class MainContentHeader {
-  #imgSrc = {
-    listBlue: 'src/images/list_blue.svg',
-    listGray: 'src/images/list_gray.svg',
-    gridBlue: 'src/images/grid_blue.svg',
-    gridGray: 'src/images/grid_gray.svg'
-  };
-
   constructor($parent) {
     this.$parent = $parent;
     this.$ele = document.createElement('header');
     this.$ele.className = 'main-content__header';
+
+    this.pressTab;
+    this.showTab;
   }
 
   mount() {
-    this.render();
-    this.setEvent();
+    this.pressTab = new PressTab(this.$ele);
+    this.showTab = new ShowTab(this.$ele);
+    this.pressTab.mount();
+    this.showTab.mount();
+
     this.$parent.insertAdjacentElement('afterbegin', this.$ele);
-    tabStore.register(this.render.bind(this));
-  }
-
-  render() {
-    this.$ele.innerHTML = this.template();
-  }
-
-  template() {
-    const { activePressTab, activeShowTab } = tabStore.getState();
-    const { listBlue, listGray, gridBlue, gridGray } = this.#imgSrc;
-
-    return /* html */ `
-      <div class="press-tab">
-        <span class="press-tab-btn press-tab__all ${
-          activePressTab === 'all' ? 'active' : ''
-        }">전체 언론사</span>
-        <span class="press-tab-btn press-tab__subscribed ${
-          activePressTab === 'subscribed' ? 'active' : ''
-        }">내가 구독한 언론사</span>
-      </div>
-      <div class="show-tab">
-        <img class="show-tab-btn show-tab__list" src="${activeShowTab === 'list' ? listBlue : listGray}">
-        <img class="show-tab-btn show-tab__grid" src="${activeShowTab === 'grid' ? gridBlue : gridGray}">
-      </div>
-    `;
-  }
-
-  setEvent() {
-    this.$ele.addEventListener('click', ({ target }) => {
-      const targetClassList = target.classList;
-      const { activePressTab, activeShowTab } = tabStore.getState();
-
-      if (targetClassList.contains('press-tab__all') && activePressTab === 'subscribed')
-        tabStore.dispatch({ type: 'togglePressTab', payload: 'all' });
-      if (targetClassList.contains('press-tab__subscribed') && activePressTab === 'all')
-        tabStore.dispatch({ type: 'togglePressTab', payload: 'subscribed' });
-      if (targetClassList.contains('show-tab__grid') && activeShowTab === 'list')
-        tabStore.dispatch({ type: 'toggleShowTab', payload: 'grid' });
-      if (targetClassList.contains('show-tab__list') && activeShowTab === 'grid')
-        tabStore.dispatch({ type: 'toggleShowTab', payload: 'list' });
-    });
   }
 }
---
+++ src/js/components/mainContent/pressTab.js
@@ -0,0 +1,51 @@
+import { tabStore } from '../../store/tabStore.js';
+
+export default class PressTab {
+  constructor($parent) {
+    this.$parent = $parent;
+    this.$ele = document.createElement('div');
+    this.$ele.className = 'press-tab';
+  }
+
+  mount() {
+    this.render();
+    this.setEvent();
+    this.$parent.insertAdjacentElement('beforeend', this.$ele);
+    tabStore.register(this.render.bind(this));
+  }
+
+  render() {
+    this.$ele.innerHTML = this.template();
+  }
+
+  template() {
+    const { activePressTab } = tabStore.getState();
+
+    return /* html */ `
+    <span class="press-tab-btn press-tab__all ${
+      activePressTab === 'all' ? 'active' : ''
+    }">전체 언론사</span>
+    <span class="press-tab-btn press-tab__subscribed ${
+      activePressTab === 'subscribed' ? 'active' : ''
+    }">내가 구독한 언론사</span>
+    `;
+  }
+
+  setEvent() {
+    this.$ele.addEventListener('click', ({ target }) => {
+      const targetClassList = target.classList;
+      const { activePressTab } = tabStore.getState();
+
+      if (
+        targetClassList.contains('press-tab__all') &&
+        activePressTab === 'subscribed'
+      )
+        tabStore.dispatch({ type: 'togglePressTab', payload: 'all' });
+      if (
+        targetClassList.contains('press-tab__subscribed') &&
+        activePressTab === 'all'
+      )
+        tabStore.dispatch({ type: 'togglePressTab', payload: 'subscribed' });
+    });
+  }
+}
