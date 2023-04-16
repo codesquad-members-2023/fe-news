ca8647d3e5995e946e110e59ea72b77b53ba9275
YouSooA <kinsk2839@naver.com>
2023-04-16 18:37:01 +0900

    feat: 페이지 이동하는 화살표 함수 추가
    - layout 스타일 적용

--- src/js/components/mainContent/mainContentGrid.js
+++ src/js/components/mainContent/mainContentGrid.js
@@ -1,6 +1,12 @@
+import { $ } from '../../utils/dom.js';
 import PressGrid from './pressGrid.js';
 
 export default class MainContentGrid {
+  #imgSrc = {
+    beforeBtn: 'src/images/before_btn.svg',
+    nextBtn: 'src/images/next_btn.svg'
+  };
+
   constructor($parent, props) {
     this.$parent = $parent;
     this.$ele = document.createElement('section');
@@ -14,9 +20,32 @@ export default class MainContentGrid {
   mount() {
     const { pressChucks } = this.props;
 
-    this.pressGridCollection = pressChucks.map((chuck) => new PressGrid(this.$ele, { gridItemsData: chuck }));
+    this.render();
+    const $gridWrapper = $({ selector: '.main-content__grid-wrapper', parent: this.$ele });
+    this.pressGridCollection = pressChucks.map(
+      (chuck) => new PressGrid($gridWrapper, { gridItemsData: chuck })
+    );
     this.pressGridCollection.forEach((pressGrid) => pressGrid.mount());
 
     this.$parent.insertAdjacentElement('beforeend', this.$ele);
+  }
+
+  render() {
+    this.$ele.innerHTML = this.template();
+  }
+
+  template() {
+    const { beforeBtn, nextBtn } = this.#imgSrc;
+
+    return `
+      <div class="main-content__grid-before-btn">
+        <img src="${beforeBtn}" alt="before grid page" />
+      </div>
+      <div class="main-content__grid-next-btn">
+        <img src="${nextBtn}" alt="next grid page" />
+      </div>
+      <div class="main-content__grid-wrapper">
+      </div>
+    `;
   }
 }
