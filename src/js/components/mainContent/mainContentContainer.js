f4cb521e18a713984234e80783fcb96f7e047a02
YouSooA <kinsk2839@naver.com>
2023-04-17 13:58:12 +0900

    refactor: MainContentContainer의 상태 제거 및 자식 컴포넌트의 props에 pressTabType 추가

--- src/js/components/mainContent/mainContentContainer.js
+++ src/js/components/mainContent/mainContentContainer.js
@@ -1,4 +1,5 @@
 import { getChucks } from '../../utils/getData.js';
+import { subscriptionListStore } from '../../store/index.js';
 import MainContentGrid from './mainContentGrid.js';
 
 export default class MainContentContainer {
@@ -10,8 +11,6 @@ export default class MainContentContainer {
     this.$ele.className = 'main-content__container';
 
     this.props = props;
-    this.state = {};
-    this.initState();
 
     this.allGrid;
     this.subscribedGrid;
@@ -19,28 +18,21 @@ export default class MainContentContainer {
 
   mount() {
     const { allPressData } = this.props;
-    const { subscriptionList } = this.state;
+    const { subscriptionList } = subscriptionListStore.getState();
     const subscribedPressData = allPressData.filter(({ pressName }) => subscriptionList.includes(pressName));
 
     const allPressChucks = getChucks({ arr: allPressData, count: this.#gridItemCount });
     const subscribedPressChucks = getChucks({ arr: subscribedPressData, count: this.#gridItemCount });
 
-    this.allGrid = new MainContentGrid(this.$ele, { pressChucks: allPressChucks });
-    this.subscribedGrid = new MainContentGrid(this.$ele, { pressChucks: subscribedPressChucks });
+    this.allGrid = new MainContentGrid(this.$ele, { pressTabType: 'all', pressChucks: allPressChucks });
+    this.subscribedGrid = new MainContentGrid(this.$ele, {
+      pressTabType: 'subscribed',
+      pressChucks: subscribedPressChucks
+    });
 
     this.allGrid.mount();
     this.subscribedGrid.mount();
 
     this.$parent.insertAdjacentElement('beforeend', this.$ele);
-  }
-
-  initState() {
-    this.state = {
-      subscriptionList: []
-    };
-  }
-
-  setState(newState) {
-    this.state = { ...this.state, ...newState };
   }
 }
