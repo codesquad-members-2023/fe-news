2473f46abb38e6e680ed7a1889f4f9006d7d2e1e
YouSooA <kinsk2839@naver.com>
2023-04-17 12:38:51 +0900

    feat: gridPageReducer, subscriptionListReducer 생성
    - tabReducer.js파일 제거하고 src/js/reducer/index.js에 모든 reducer 함수 관리

---
+++ src/js/reducer/index.js
@@ -0,0 +1,35 @@
+export const tabReducer = (state, action) => {
+  switch (action.type) {
+    case 'togglePressTab': {
+      return { ...state, activePressTab: action.payload };
+    }
+    case 'toggleShowTab': {
+      return { ...state, activeShowTab: action.payload };
+    }
+    default:
+      return state;
+  }
+};
+
+export const gridPageReducer = (state, action) => {
+  switch (action.type) {
+    case 'initGridPage': {
+      const { pressTabType, currentPage, totalPages } = action.payload;
+      return { ...state, [pressTabType]: { currentPage, totalPages } };
+    }
+    case 'nextPage': {
+      const { pressTabType, currentPage, totalPages } = action.payload;
+      const newPage = currentPage + 1 === totalPages ? currentPage : currentPage + 1;
+      return { ...state, [pressTabType]: { currentPage: newPage, totalPages } };
+    }
+    case 'beforePage': {
+      const { pressTabType, currentPage, totalPages } = action.payload;
+      const newPage = currentPage - 1 === -1 ? 0 : currentPage - 1;
+      return { ...state, [pressTabType]: { currentPage: newPage, totalPages } };
+    }
+    default:
+      return state;
+  }
+};
+
+export const subscriptionListReducer = (state, action) => {};
