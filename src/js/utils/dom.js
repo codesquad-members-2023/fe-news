a90e74665c6d277aeae1e3734dd1904e723ad7e8
YouSooA <kinsk2839@naver.com>
2023-04-19 00:08:55 +0900

    refactor: utils/dom.js와 utils/getData.js 파일 삭제

--- src/js/utils/dom.js
+++
@@ -1,4 +0,0 @@
-export const $ = ({ selector, parent = document }) =>
-  parent.querySelector(selector);
-export const $$ = ({ selector, parent = document }) =>
-  parent.querySelectorAll(selector);
