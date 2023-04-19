2d16c580af110f8b3a0a4acdfe0abf17357fe2aa
saseung <seung0122@naver.com>
2023-04-19 15:41:23 +0900

    refactor: fetch함수 병렬로 데이터 반환할 수 있도록 구현
    - promise.all을 사용하여 url을 하나씩 반환하는게 아닌 여러개의 url을 하나의 배열로 묶어서 반환할 수 있도록 수정
    - app.js 코드 가독성있게 수정

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,18 @@
-import { viewHeader } from './components/header/header.js';
-import { Headline } from './components/headline/headline.js';
-import { headLineData, mediaData } from './api/fetchData.js';
-import { Main } from './components/main/main.js';
+import fetchData from './utils/dataFetcher.js';
+import createHeader from './components/header/header.js';
+import Headline from './components/headline/headline.js';
+import Main from './components/main/main.js';
 
-const App = () => {
+const App = async () => {
   const root = document.querySelector('#NEWSSTAND');
 
-  const header = viewHeader();
+  const [headlineData, mainData] = await fetchData(['headline', 'media']);
 
-  const headline = new Headline(headLineData);
-  const main = new Main(mediaData);
+  const header = createHeader();
+  const headline = new Headline(headlineData).render();
+  const main = new Main(mainData).render();
 
-  root.append(header, headline.render(), main.render());
+  root.append(header, headline, main);
 };
 
 App();
