ee965b491933eededdcf3752edf4a44b6e4ac8f5
poco <godiva140@gmail.com>
2023-04-20 11:44:09 +0900

    feat: grid 구독 보기 화면에서 해제하기 버튼 클릭 이벤트 구현

--- src/js/view/gridSubView.js
+++ src/js/view/gridSubView.js
@@ -1,6 +1,9 @@
 import { REFERENCE, NS_SECTION_INFO } from '../constant/dom.js';
 
 export default class GridSubView {
+  _gridPress;
+  _pressName;
+  _noSubPopupWrap;
   constructor(gridSubModel) {
     this._gridSubModel = gridSubModel;
     this._gridSubModel.subscribe(this.render.bind(this));
@@ -11,6 +14,7 @@ export default class GridSubView {
     const parentElem = REFERENCE.NS_CONTAINER.querySelector('.newssection_view');
     parentElem.innerHTML = '';
     parentElem.insertAdjacentHTML('afterbegin', gridSubSection);
+    this.setEvent();
   }
 
   getMarkUp(data) {
@@ -20,15 +24,50 @@ export default class GridSubView {
       data.reduce((acc, cur) => {
         acc += `<div class="grid_items">
         <a class="grid_press"> <img class="press_logo" src="${cur.logoImgSrc}" alt="${cur.pressName}"/> </a>
-        <div class="popup_wrap"></div>
+        <a class="nosub_popup_wrap hidden"><img class="nosub_button" src="/src/asset/newsSectionGridNoSubButton.svg"></a>
       </div>`;
         return acc;
       }, `<div class="newssection_view_grid">`) +
       emptyData.reduce((acc, cur) => {
-        acc += `<div class="grid_items"></div>`;
+        acc += `<div class="grid_items empty"></div>`;
         return acc;
       }, ``) +
       `</div>`
     );
+  }
+
+  setSelectedGridSection(gridContainer) {
+    this._gridPress = gridContainer.querySelector('.grid_press');
+    this._pressName = this._gridPress.querySelector('.press_logo').alt;
+    this._subPopupWrap = gridContainer.querySelector('.sub_popup_wrap');
+    this._noSubPopupWrap = gridContainer.querySelector('.nosub_popup_wrap');
+  }
+
+  gridSectionMouseOverEventHandler({ target }) {
+    const gridContainer = target.closest('.grid_items');
+    if (!gridContainer || target.classList.contains('empty')) return;
+
+    this.setSelectedGridSection(gridContainer);
+    this._gridPress.classList.add('hidden');
+    this._noSubPopupWrap.classList.remove('hidden');
+  }
+
+  gridSectionMouseOutEventHandler({ target }) {
+    const gridContainer = target.closest('.grid_items');
+    if (!gridContainer || target.classList.contains('empty')) return;
+    this._gridPress.classList.remove('hidden');
+    this._noSubPopupWrap.classList.add('hidden');
+  }
+
+  gridSectionClickEventHandler({ target }) {
+    if (!target.classList.contains('nosub_button')) return;
+    this._gridSubModel.deleteSubData(this._pressName);
+  }
+
+  setEvent() {
+    const parentElem = REFERENCE.NS_CONTAINER.querySelector('.newssection_view_grid');
+    parentElem.addEventListener('mouseover', this.gridSectionMouseOverEventHandler.bind(this));
+    parentElem.addEventListener('mouseout', this.gridSectionMouseOutEventHandler.bind(this));
+    parentElem.addEventListener('click', this.gridSectionClickEventHandler.bind(this));
   }
 }
