cd6d4b451288c986085990044c6db720b68aac2f
JangHoonSeok <orirori512@naver.com>
2023-04-26 17:41:18 +0900

    ✨ feat: 프로그레스바 애니메이션 일시정지를 위한 rAF 함수 조작

--- js/components/main/progressBarAnimation.js
+++ js/components/main/progressBarAnimation.js
@@ -8,28 +8,35 @@ const ANIMATION = {
 
 export const animationStart = ($progressBar) => {
   if (!$progressBar) return;
+
   const viewOption = getStoreState('viewOptionData').viewOption;
   let startTime = null;
 
   const progressBarAnimation = (timestamp) => {
+    const { durationElapsed, isPaused } = getStoreState('animationData');
     if (startTime === null) startTime = timestamp;
-    const duration = timestamp - startTime;
+    const duration = timestamp - startTime + durationElapsed;
 
-    if (duration <= ANIMATION.DURATION) {
-      $progressBar.style.background = `linear-gradient(90deg, #4362d0 ${
-        (duration / ANIMATION.DURATION) * ANIMATION.MAKE_PERCENT
-      }%, #7890e7 0%)`;
-      dispatch(
-        displayActionCreator.progressBarAnimationStart(
-          requestAnimationFrame(progressBarAnimation),
-        ),
-      );
-    } else {
-      if (viewOption.allOrMine === 'all') {
-        dispatch(displayActionCreator.listRightBtnClick());
-      } else if (viewOption.allOrMine === 'mine') {
-        dispatch(displayActionCreator.mineListRightBtnClick());
+    if (!isPaused) {
+      if (duration <= ANIMATION.DURATION) {
+        $progressBar.style.background = `linear-gradient(90deg, #4362d0 ${
+          (duration / ANIMATION.DURATION) * ANIMATION.MAKE_PERCENT
+        }%, #7890e7 0%)`;
+        dispatch(
+          displayActionCreator.progressBarAnimationStart(
+            requestAnimationFrame(progressBarAnimation),
+          ),
+        );
+      } else {
+        if (viewOption.allOrMine === 'all') {
+          dispatch(displayActionCreator.listRightBtnClick());
+        } else if (viewOption.allOrMine === 'mine') {
+          dispatch(displayActionCreator.mineListRightBtnClick());
+        }
       }
+    } else {
+      dispatch(displayActionCreator.progressBarAnimationEnd());
+      dispatch(displayActionCreator.progressBarDurationSave(duration));
     }
   };
   dispatch(
