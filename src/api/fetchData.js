bb8a40ac6a1d6e54dffbc661ba341e403057bdd6
sarang_daddy <sarang_daddy@naver.com>
2023-04-27 16:18:00 +0900

    [Docs] Rest 설치, Babel 셋팅

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,9 +1,14 @@
 export const dataRequestToAPI = async (URL) => {
   try {
     const response = await fetch(URL);
+    if (!response.ok) {
+      // 요청은 성공했지만 서버에서 오유가 발생한 경우
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
     const jsonData = await response.json();
     return jsonData;
   } catch (error) {
-    console.error(`fetch 에러! ${error}`);
+    // 네트워크 요청에 문제가 있는 경우 예외 처리
+    throw new Error(`fetch 에러! ${error}`);
   }
 };
