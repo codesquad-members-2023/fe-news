68fdc88f671dbbfae7332c0d74cda2eee34fb162
YouSooA <kinsk2839@naver.com>
2023-05-01 00:58:46 +0900

    feat: 그리드의 좌우 화살표 이벤트 적용
    - 좌 화살표를 클릭했을 때, 다음 페이지로 이동
    - 우 화살표를 클릭했을 때, 이전 페이지로 이동
    - 가장 첫 페이지일 때, 가장 왼쪽 화살표는 표시되지 않음
    - 가장 마지막 페이지일 때, 가장 오른쪽 화살표는 표시되지 않음

--- src/js/components/mainContent/mainContentGrid.js
+++ src/js/components/mainContent/mainContentGrid.js
@@ -1,13 +1,26 @@
-import { gridStore } from '../../store/index.js';
+import { gridStore, GRID_ACTION_TYPES, subscriptionStore } from '../../store/index.js';
 
 const isFirstPage = (currentPage) => currentPage === 0;
 const isLastPage = (currentPage, totalPages) => currentPage === totalPages - 1;
+const getDataSlices = ({ arr, count = 1 }) => {
+  const dataSlices = [];
+
+  for (let i = 0; i < arr.length; i += count) {
+    dataSlices.push(arr.slice(i, i + count));
+  }
+
+  return dataSlices;
+};
 
 export default class MainContentGrid {
+  #itemCount = 24;
+
   #imgSrc = {
     beforeBtn: 'src/images/before_btn.svg',
     nextBtn: 'src/images/next_btn.svg'
   };
+
+  #dataSlices;
 
   constructor($parent, props) {
     this.$parent = $parent;
@@ -18,12 +31,41 @@ export default class MainContentGrid {
 
     this.children = new Set();
     this.$parent.insertAdjacentElement('beforeend', this.$mainEle);
+
+    const { pressData, activePressTab } = this.props;
+    this.initGrid(pressData, activePressTab);
+
+    this.unregisterGrid = gridStore.register(() => {
+      this.displayBtn();
+    });
+  }
+
+  initGrid(pressData, activePressTab) {
+    this.setDataSlices(pressData, activePressTab);
+    const totalPages = this.getTotalPages();
+
+    gridStore.dispatch({
+      type: GRID_ACTION_TYPES.INIT_STATE,
+      payload: { pressTab: activePressTab, totalPages }
+    });
+  }
+
+  setDataSlices(pressData, activePressTab) {
+    const { subscriptionList } = subscriptionStore.getState();
+    const selectedPressData = activePressTab === 'all' ? pressData : subscriptionList;
+
+    this.#dataSlices = getDataSlices({ arr: selectedPressData, count: this.#itemCount });
+  }
+
+  getTotalPages() {
+    return this.#dataSlices.length === 0 ? 1 : this.#dataSlices.length;
   }
 
   render() {
     this.removeChildren();
 
     this.$mainEle.innerHTML = this.template();
+    this.setEvent();
   }
 
   template() {
@@ -40,6 +82,34 @@ export default class MainContentGrid {
       <div class="main-content__grid-wrapper">
       </div>
     `;
+  }
+
+  setEvent() {
+    const actionByTarget = {
+      'grid-before-btn': GRID_ACTION_TYPES.BEFORE_PAGE,
+      'grid-next-btn': GRID_ACTION_TYPES.NEXT_PAGE
+    };
+
+    this.$mainEle.addEventListener('click', ({ target }) => {
+      const { id } = target;
+
+      if (actionByTarget[id]) {
+        gridStore.dispatch({ type: actionByTarget[id] });
+      }
+    });
+  }
+
+  displayBtn() {
+    const { currentPage, totalPages } = gridStore.getState();
+
+    const $beforeBtn = this.$mainEle.querySelector('.main-content__grid-before-btn');
+    const $nextBtn = this.$mainEle.querySelector('.main-content__grid-next-btn');
+
+    if (isFirstPage(currentPage)) $beforeBtn.classList.add('hidden');
+    else $beforeBtn.classList.remove('hidden');
+
+    if (isLastPage(currentPage, totalPages)) $nextBtn.classList.add('hidden');
+    else $nextBtn.classList.remove('hidden');
   }
 
   remove() {
